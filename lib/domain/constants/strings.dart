419d63ff60b2bcc8050cfb7f7b3ede4745507e5d
Wesley Bliss <wesley.bliss@gmail.com>
2025-03-02 20:19:37 -0500

    Cleanup

--- lib/domain/constants/strings.dart
+++ lib/domain/constants/strings.dart
@@ -3,16 +3,12 @@ class ConstantsStrings {
   final appSlug = 'simple-currency';
 
   String get baseUrl {
-    String base;
-
-    // Use const bool.fromEnvironment to check build mode
     const bool isProduction = bool.fromEnvironment('dart.vm.product');
 
     if (isProduction) {
-      base = 'https://simple-currency-cron.vercel.app/api';
+      return 'https://simple-currency-cron.vercel.app/api';
     } else {
-      base = 'http://localhost:3001/api';
+      return 'http://localhost:3001/api';
     }
-    return base;
   }
 }
