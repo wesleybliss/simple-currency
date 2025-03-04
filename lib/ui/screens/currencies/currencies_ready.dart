218b6df35d6a82b163754277dd452551bbe9c3a4
Wesley Bliss <wesley.bliss@gmail.com>
2025-03-04 11:53:37 -0500

    Made entire row/tile clickable when selecting favorite currencies

--- lib/ui/screens/currencies/currencies_ready.dart
+++ lib/ui/screens/currencies/currencies_ready.dart
@@ -6,7 +6,7 @@ import 'package:simple_currency/utils/logger.dart';
 
 class CurrenciesReady extends ConsumerStatefulWidget {
   const CurrenciesReady({super.key});
-  
+
   @override
   ConsumerState<CurrenciesReady> createState() => _CurrenciesReadyState();
 }
@@ -14,23 +14,25 @@ class CurrenciesReady extends ConsumerStatefulWidget {
 class _CurrenciesReadyState extends ConsumerState<CurrenciesReady> {
   final log = Logger('CurrenciesReady');
   final controller = TextEditingController();
-  
+
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(currenciesProvider);
     final searchText = controller.text.toLowerCase();
-    
-    final filteredCurrencies = searchText.isEmpty ? state.currencies : state.currencies.where((it) {
-      final currencyName = it.name.toLowerCase();
-      final currencySymbol = it.symbol.toLowerCase();
-      return currencyName.contains(searchText) || currencySymbol.contains(searchText);
-    }).toList();
+
+    final filteredCurrencies = searchText.isEmpty
+        ? state.currencies
+        : state.currencies.where((it) {
+            final currencyName = it.name.toLowerCase();
+            final currencySymbol = it.symbol.toLowerCase();
+            return currencyName.contains(searchText) || currencySymbol.contains(searchText);
+          }).toList();
 
     filteredCurrencies.sort((a, b) {
       if (a.selected && !b.selected) {
@@ -41,29 +43,37 @@ class _CurrenciesReadyState extends ConsumerState<CurrenciesReady> {
         return a.symbol.compareTo(b.symbol); // Both are selected or both are not selected
       }
     });
-    
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-      const Padding(padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0), child:
-      Text('Select Favorite Currencies',
-        textAlign: TextAlign.left,
-        style: TextStyle(fontSize: 16),
-      )),
 
-    Padding(padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0), child: TextField(
-        controller: controller,
-        decoration: const InputDecoration(
-          border: UnderlineInputBorder(),
-          labelText: 'Search Currencies',
-        ),
-        onChanged: (text) {
-          setState(() {});
-        },
-      )),
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+          child: Text(
+            'Choose your favorite currencies.\nThese will be pinned to the home screen.',
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: 16),
+          )),
+      Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
+          child: TextField(
+            controller: controller,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Search by name or symbol (e.g. USD)',
+            ),
+            onChanged: (text) {
+              // Empty state update to trigger the change & filter the list
+              setState(() {});
+            },
+          )),
       const SizedBox(height: 20),
       Expanded(
-        child: CurrenciesList(currencies: filteredCurrencies),
-      )]);
+        child: CurrenciesList(
+            currencies: filteredCurrencies,
+            onFavoriteToggled: () {
+              controller.clear();
+              setState(() {});
+            }),
+      )
+    ]);
   }
 }
